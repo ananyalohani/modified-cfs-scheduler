e073590b57375064f246d29942f75322be3bff3d
Ananya Lohani <ananya19018@iiitd.ac.in>
2020-11-08 12:31:43 +0000

    added rt_nice

---
+++ rt_nice.c
@@ -0,0 +1,21 @@
+SYSCALL_DEFINE2(rt_nice, long, pid, long, rt_nice)
+{
+    if(pid < 1 || pid > 2147483647)
+	{
+		printk(KERN_ALERT "Invalid value of PID.\n");
+		return -EINVAL;			// invalid argument
+	}
+
+    unsigned long long int val = (unsigned long long int)rt_nice * 1000000000;
+    struct task_struct *task = pid_task(find_vpid(pid), PIDTYPE_PID);
+
+    if(task == NULL) 
+    {
+        printk(KERN_ALERT "No such task with PID = %ld\n.", pid);
+		return -ESRCH;			// no such process
+    }
+    struct sched_entity *task_se = &(task->se);
+    task_se->rt_nice = val;
+    printk(KERN_INFO "rt_nice value modified successfully.\n");
+    return 0;
+}
